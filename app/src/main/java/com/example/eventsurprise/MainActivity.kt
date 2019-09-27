fa072a2eaa1c3b29ece31f3491687d28392b589e
goodnic <good.nicola94@gmail.com>
2019-09-28 00:27:02 +0200

    Adding map activity

--- app/src/main/java/com/example/eventsurprise/MainActivity.kt
+++ app/src/main/java/com/example/eventsurprise/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.eventsurprise
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeToMap(view: View) {
+        val changeToMapIntent = Intent(this, MapsActivity::class.java)
+        startActivity(changeToMapIntent)
     }
 }
