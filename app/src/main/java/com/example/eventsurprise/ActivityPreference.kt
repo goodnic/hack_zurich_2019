75d2cdc7531e638615f2e58d159e2eb1e7626d45
johanneskopf <johannes.kopf@student.tugraz.at>
2019-09-29 00:59:54 +0200

    finishing touches

--- app/src/main/java/com/example/eventsurprise/ActivityPreference.kt
+++ app/src/main/java/com/example/eventsurprise/ActivityPreference.kt
@@ -1,12 +1,21 @@
 package com.example.eventsurprise
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_preference.*
 
 class ActivityPreference : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preference)
+
+        btnConfirm.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java).apply {
+            }
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/eventsurprise/SurpriseActivity.kt
+++ app/src/main/java/com/example/eventsurprise/SurpriseActivity.kt
@@ -5,8 +5,21 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_surprise.*
+import kotlin.random.Random
 
 class SurpriseActivity : AppCompatActivity() {
+
+    var activitiesArr = arrayOf("Hiking",
+        "Shopping",
+        "Cycling",
+        "Music",
+        "History",
+        "Art",
+        "Gastronomy",
+        "Water Sports",
+        "Technology",
+        "Animals",
+        "Clubbing")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +30,14 @@ class SurpriseActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        btnResurprise.setOnClickListener{ view ->
+            val rand = Random.nextInt(activitiesArr.size - 1)
+            btnActivity.text = activitiesArr[rand]
+
+            btnPeopleCount.text = "" + (rand + 1) + "                                             "
+        }
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
