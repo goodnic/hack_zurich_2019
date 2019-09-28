c5b32d17a7f840c540a48998b3c1419e41302b3a
goodnic <good.nicola94@gmail.com>
2019-09-28 20:58:18 +0200

    Moving loading to intermediate activity

--- app/src/main/java/com/example/eventsurprise/IntermediateActivity.kt
+++ app/src/main/java/com/example/eventsurprise/IntermediateActivity.kt
@@ -4,12 +4,39 @@ import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class IntermediateActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intermediate)
+        // fetch data from service in the background
+        runBlocking {
+            withContext(Dispatchers.IO) {
+                var resultList = getTimeMap(LatLng(1.0, 1.0))
+                resultList.results.forEach {
+                    Log.d("MAP", "ONE OF THE RESULTS: " + it.search_id)
+                    it.shapes.forEach{
+                        val pois = getPOIs(it.shell)
+                        Log.d("POI", pois.toString())
+                    }
+                }
+                // update UI here
+                runOnUiThread {
+                    Log.d("MAP", "runOnUiThread - yeah!")
+                }
+
+                val authToken = getAuthToken()
+                Log.d("MAP", "authToken: " + authToken)
+                val locations = getLocationData(authToken)
+                Log.d("MAP", "locations : " + locations)
+            }
+        }
     }
 
     fun startActivity() {
--- app/src/main/java/com/example/eventsurprise/MainActivity.kt
+++ app/src/main/java/com/example/eventsurprise/MainActivity.kt
@@ -3,16 +3,10 @@ package com.example.eventsurprise
 import android.content.Intent
 import android.os.Bundle
 import android.transition.Slide
-import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.view.animation.DecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.model.LatLng
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,38 +15,12 @@ class MainActivity : AppCompatActivity() {
         setAnimation()
         setContentView(R.layout.activity_main)
 
-        // fetch data from service in the background
-        runBlocking {
-            withContext(Dispatchers.IO) {
-                var resultList = getTimeMap(LatLng(1.0, 1.0))
-                resultList.results.forEach {
-                    Log.d("MAP", "ONE OF THE RESULTS: " + it.search_id)
-                    it.shapes.forEach{
-                        val pois = getPOIs(it.shell)
-                        Log.d("POI", pois.toString())
-                    }
-                }
-                // update UI here
-                runOnUiThread {
-                    Log.d("MAP", "runOnUiThread - yeah!")
-                }
 
-                val authToken = getAuthToken()
-                Log.d("MAP", "authToken: " + authToken)
-                val locations = getLocationData(authToken)
-                Log.d("MAP", "locations : " + locations)
-            }
-        }
     }
 
     fun changeToIntermediate(view: View) {
         val changeToIntermediateIntent = Intent(this, IntermediateActivity::class.java)
         startActivity(changeToIntermediateIntent)
-    }
-
-    fun changeToMap(view: View) {
-        val changeToMapIntent = Intent(this, MapsActivity::class.java)
-        startActivity(changeToMapIntent)
     }
 
     fun setAnimation() {
