e51152a4acbc3ce338d3a2a5407e0c7ccb1f98fb
johanneskopf <johannes.kopf@student.tugraz.at>
2019-09-28 23:23:41 +0200

    restyled main and surprise page

---
+++ app/src/main/java/com/example/eventsurprise/BuyPage.kt
@@ -0,0 +1,12 @@
+package com.example.eventsurprise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuyPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_page)
+    }
+}
--- app/src/main/java/com/example/eventsurprise/MainActivity.kt
+++ app/src/main/java/com/example/eventsurprise/MainActivity.kt
@@ -10,6 +10,12 @@ import android.os.Bundle
 import android.transition.Slide
 import android.view.Gravity
 import android.view.View
+import android.widget.Toast
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import android.view.animation.DecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -26,6 +32,12 @@ class MainActivity : AppCompatActivity() {
         setAnimation()
         setContentView(R.layout.activity_main)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        btnSurpriseMe.setOnClickListener{
+            val intent = Intent(this, SurpriseActivity::class.java).apply {
+            }
+            startActivity(intent)
+        }
     }
 
     override fun onStart() {
